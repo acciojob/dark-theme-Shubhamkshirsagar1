8c4354529fe725127e1f961097360ff4fb94909e
shubham kshirsagar <117492782+Shubhamkshirsagar1@users.noreply.github.com>
2023-05-06 08:39:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-//your code here
+let swapButton = document.getElementById('swap');
+let appDiv = document.getElementById('app');
 
+function swapTheme() {
+  if (appDiv.classList.contains('day')) {
+    appDiv.classList.remove('day');
+    appDiv.classList.add('night');
+    swapButton.classList.remove('button_day');
+    swapButton.classList.add('button_night');
+  } else {
+    appDiv.classList.remove('night');
+    appDiv.classList.add('day');
+    swapButton.classList.remove('button_night');
+    swapButton.classList.add('button_day');
+  }
+}
+
+swapButton.addEventListener('click', swapTheme);
